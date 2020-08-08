8b8da0798dd6bcffafe9ec18f4734c2c53ab4c5c
Himanshu Bhanpurkar <hbhanpurkar@gmail.com>
2020-08-08 20:39:44 +0530

    added timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 
-
-const Task = mongoose.model('Task',{
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         trim: true,
@@ -17,6 +16,11 @@ const Task = mongoose.model('Task',{
         Required: true,
         ref: 'User'
     }
+},{
+    timestamps: true
 })
+
+
+const Task = mongoose.model('Task',taskSchema)
 
 module.exports = Task
